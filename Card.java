fed48ba6f705e6b6209636ef20130f622b8d23b4
jnbmaster <jnb.cb38@gmail.com>
2018-04-25 19:41:24 -0400

    sfsdf

--- Card.java
+++ Card.java
@@ -1,8 +1,43 @@
 public class Card {
 	
+	private String type;
+	private int num;
+	
 	public Card(String type, int num) 
 	{
-		
+		setType(type);
+		setNum(num);
+	}
+
+	String getType() 
+	{
+		return type;
+	}
+
+	int getNum() 
+	{
+		return num;
+	}
+
+	void setType(String type) 
+	{
+		switch(type)
+		{
+			case CardType.CLUBS: this.type = type; break;
+			case CardType.DIAMONDS: this.type = type; break;
+			case CardType.SPADES: this.type = type; break;
+			case CardType.HEARTS: this.type = type; break;
+			default: throw new RuntimeException("Invalid Card Value");
+		}
+	}
+
+	void setNum(int num) 
+	{
+		if (num < 0 || num > 13)
+		{
+			throw new RuntimeException("Invalid Card Value");
+		}
+		this.num = num;
 	}
 	
 }
--- Deck.java
+++ Deck.java
@@ -1,4 +1,62 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Stack;
+
 public class Deck 
 {
+	private Stack<Card> deck = new Stack<>();
 	
+	public Deck()
+	{
+		newGame();
+	}
+	
+	private void shuffle() 
+	{
+		Collections.shuffle(deck);
+	}
+
+	private void createDeck()
+	{
+		Card card;
+		for (int index = 1; index <= 13; index++)
+		{
+			card = new Card(CardType.CLUBS, index);
+			deck.add(card);
+		}
+		for (int index = 1; index <= 13; index++)
+		{
+			card = new Card(CardType.SPADES, index);
+			deck.add(card);
+		}
+		for (int index = 1; index <= 13; index++)
+		{
+			card = new Card(CardType.HEARTS, index);
+			deck.add(card);
+		}
+		for (int index = 1; index <= 13; index++)
+		{
+			card = new Card(CardType.DIAMONDS, index);
+			deck.add(card);
+		}
+	}
+	
+	public ArrayList<Card> dealHand()
+	{
+		ArrayList<Card> hand = new ArrayList<>();
+		hand.add(deck.pop());
+		hand.add(deck.pop());		
+		return hand;
+	}
+	
+	public Card dealCard()
+	{
+		return deck.pop();
+	}
+	
+	public void newGame()
+	{
+		createDeck();
+		shuffle();
+	}
 }
